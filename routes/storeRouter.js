fb7e3593b88d43efc86a2ff0cbe33b4eeb19a667
ahmed isiaka <ahmeddavids6@gmail.com>
2025-01-25 20:22:25 +0100

    s..

---
+++ routes/storeRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { createStore, getAllStores, getOneStore } = require('../controllers/storeController');
+
+const router = express.Router();
+
+
+module.exports = router
